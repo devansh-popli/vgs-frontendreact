9019882534ea310f381401efe4c4a94c42a6a776
devansh <dvpopli@gmail.com>
2024-11-15 23:50:57 +0530

    cosmetic changes

--- src/components/users/SingleProductView.jsx
+++ src/components/users/SingleProductView.jsx
@@ -51,9 +51,9 @@ const SingleProductView = ({ product, width }) => {
             : product?.categories[0]?.title.substring(0, 20) + "..."}
         </h6> */}
         <h6 className="ms-2 text-capitalize text-gray font"  onClick={navigateToProductsView}>
-          {product.title && product.title?.length < 22
+          {product.title && product.title?.length < 19
             ? product?.title
-            : product?.title.substring(0, 22) + "..."}
+            : product?.title.substring(0, 19) + "..."}
         </h6>
         <Container className="text-start font">
           <b>
